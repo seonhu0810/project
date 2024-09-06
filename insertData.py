1fd660d8c24320698640abd5e7860b962fc475f7
Clotted <junealsk@naver.com>
2024-09-06 10:54:57 +0900

    입력 수정 파일 수정

--- insertData.py
+++ insertData.py
@@ -1,11 +1,11 @@
 from _mysql_connector import Error
 from connect import connect
 
-#이름 책이름 isbm
+#이름 책이름 id
 
-def insert_data(name, bookname, isbm):
-    query = "INSERT INTO users (name, bookname, isbm) VALUES (%s, %s, %s)" 
-    data = (name, bookname, isbm)
+def insert_data(name, bookname, id):
+    query = "INSERT INTO users (name, bookname, id) VALUES (%s, %s, %s)" 
+    data = (name, bookname, id)
 
     try:
         # connect 함수로 데이터베이스에 연결
--- updateData.py
+++ updateData.py
@@ -1,10 +1,11 @@
 from mysql.connector import Error
 from connect import connect
 
-def update_customer(customer_id, name, bookname, isbm):
-    """고객 데이터를 업데이트하는 함수"""
-    query = "UPDATE users SET name = %s, bookname = %s, isbm = %s WHERE id = %s"
-    data = (name, bookname, isbm, customer_id)
+def update_customer(name, bookname, id):
+
+    #고객 데이터를 업데이트하는 함수
+    query = "UPDATE users SET name = %s, bookname = %s, id = %s"
+    data = (name, bookname, id,)
     
     try:
         conn = connect()  # 데이터베이스에 연결
@@ -32,6 +33,4 @@ def update_customer(customer_id, name, bookname, isbm):
             conn.close()
             print("데이터베이스 연결이 종료되었습니다.")
 
-if __name__ == "__main__":
-    # 예시로 데이터 업데이트
-    update_customer(1, "Jane Doe", "New Book Name", "1234567890")
+
